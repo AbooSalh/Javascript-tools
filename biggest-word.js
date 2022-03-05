55f872fc02d0761fb7f990c62e88c3ffcf75bf43
AbooSalh <a.m0507979@gmail.com>
2022-03-05 18:58:44 +0200

    add new var

--- biggest-word.js
+++ biggest-word.js
@@ -1,4 +1,5 @@
-let lest = ["your","paragraph","or","words"]
+let sentence = "your paragraph  or words"
+let lest = sentence.trim().split(" ")
 let check = lest.reduce((acc,current)=>{
     return acc.length > current.length ? acc : current;
 })
